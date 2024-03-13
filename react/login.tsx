fc835ca75c8ed9952b296b4db0f9d02dd16d5f6f
Justin Alexander <98443102+im7ven@users.noreply.github.com>
2024-03-13 16:21:02 -0400

    Update login.tsx
    Add functionality to access the user inputs

--- react/login.tsx
+++ react/login.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React {useState} from "react";
 
 import useLogin from '/react/api/useLoggin';
 
 import Button from '.'
 import Input from '.'
 
+type User = {
+  userName: string;
+  password: string;
+}
+  
+  
 function Login() {
+
+  const [user, setUser] = useState<User>({userName:"", password: ""})
 
   return (<div>
     <Input 
       value={'username'} 
-      onChange={() => {}}
+      onChange={(value) => {setUser({...user, userName: value})}}
     />
     <Input
       value={'password'}
+      onChange={(value)  => {setUser({...user, password: value})}}
       secret
     />
     <Button onClick={() => {}} disabled>
