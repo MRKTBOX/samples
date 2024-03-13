0a448cd3092e3424de2e3f8132b9ba957cd70d7d
josizhou <56170620+josizhou@users.noreply.github.com>
2024-03-13 07:16:02 -0700

    Update login.tsx
    Fixed Login formatting

--- react/login.tsx
+++ react/login.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-
 import useLogin from '/react/api/useLoggin';
-
-import Button from '.'
-import Input from '.'
+import Button from '.';
+import Input from '.';
 
 function Login() {
 
-  return (<div>
-    <Input 
-      value={'username'} 
-      onChange={() => {}}
-    />
-    <Input
-      value={'password'}
-      secret
-    />
-    <Button onClick={() => {}} disabled>
-      Submit
-    </Button>
-  </div>
+  return (
+    <div>
+      <Input
+        value={'username'} 
+        onChange={() => {}}
+      />
+      <Input
+        value={'password'}
+        secret
+      />
+      <Button onClick={() => {}} disabled>
+        Submit
+      </Button>
+    </div>
   )
 }
 
