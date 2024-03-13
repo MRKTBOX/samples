14ebb7071ac1ae9787e291ec5ca13178e58a0523
Leon <leon.karho.chung@gmail.com>
2024-03-13 15:17:09 -0400

    Feature: handle on change functions for username and password inputs - set states respectively

--- react/login.tsx
+++ react/login.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import useLogin from '/react/api/useLoggin.ts';
 import Button from './Button.ts'
 import Input from './Input.ts'
 
 function Login() {
-
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  
   return (
     <div>
       <Input 
-        value={'username'} 
-        onChange={() => {}}
+        value={username} 
+        onChange={(username) => {
+          setUsername(username)
+        }}
       />
       <Input
-        value={'password'}
+        value={password}
         secret
+        onChange={(password) => {
+          setPassword(password)
+        }}
       />
       <Button onClick={() => {}} disabled>
         Submit
