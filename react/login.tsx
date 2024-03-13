ee89a4a92781d0dc5b22e0cc78263bf9e0ab3751
josizhou <56170620+josizhou@users.noreply.github.com>
2024-03-13 07:20:44 -0700

    Update login.tsx
    Implemented userName and password states and integrated with Inputs

--- react/login.tsx
+++ react/login.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import useLogin from '/react/api/useLoggin';
 import Button from '.';
 import Input from '.';
 
 function Login() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <div>
       <Input
-        value={'username'} 
-        onChange={() => {}}
+        value={userName} 
+        onChange={(value) => {setUserName(value)}}
       />
       <Input
-        value={'password'}
+        value={password}
+        onChange={(value) => {setPassword(value)}}
         secret
       />
       <Button onClick={() => {}} disabled>
