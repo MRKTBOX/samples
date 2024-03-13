5ba3de21891edb267fc29ac42e34abec94e90c1a
Leon <leon.karho.chung@gmail.com>
2024-03-13 15:11:41 -0400

    enhancement: fix syntax and properly import files

--- react/login.tsx
+++ react/login.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-
-import useLogin from '/react/api/useLoggin';
-
-import Button from '.'
-import Input from '.'
+import useLogin from '/react/api/useLoggin.ts';
+import Button from './Button.ts'
+import Input from './Input.ts'
 
 function Login() {
 
-  return (<div>
-    <Input 
-      value={'username'} 
-      onChange={() => {}}
-    />
-    <Input
-      value={'password'}
-      secret
-    />
-    <Button onClick={() => {}} disabled>
-      Submit
-    </Button>
-  </div>
+  return (
+    <div>
+      <Input 
+        value={'username'} 
+        onChange={() => {}}
+      />
+      <Input
+        value={'password'}
+        secret
+      />
+      <Button onClick={() => {}} disabled>
+        Submit
+      </Button>
+    </div>
   )
 }
 
