cc40d1e505ea212933821b6ddf69a79fc7285242
Harrison Kim <harrisonkimdev@gmail.com>
2024-03-13 11:16:23 -0400

    Add states for username and password in the Login.tsx.

--- react/login.tsx
+++ react/login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import useLogin from '/react/api/useLoggin';
 
@@ -6,20 +6,24 @@ import Button from '.'
 import Input from '.'
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   return (<div>
     <form>
       <lable>username</lable>
       <Input 
-        value={'username'} 
-        onChange={() => {}}
-        placeholder="Please enter your username."
+        value={username} 
+        onChange={(e) => { setUsername(e.target.value) }}
+        disabled={false}
+        secret={false}
       />
       <label>password</label>
       <Input
-        value={'password'}
-        secret
-        placeholder="Please enter your password."
+        value={password}
+        onChange={(e) => { setPassword(e.target.value) }}
+        disabled={false}
+        secret={true}
       />
       <Button type="submit" onClick={() => {}} disabled>
         Submit
